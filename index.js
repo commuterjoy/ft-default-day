609576256d285dab67295bfc0ce8a5333086f833
Matt Chadburn <matt.chadburn@ft.com>
2016-10-04 23:06:12 +0100

    Fix res

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var app = express();
 
 app.use(serveStatic(__dirname));
 
-app.get('/', () => fs.readFileSync(__dirname + '/index.html', 'utf-8'));
-app.get('/nps', () => fs.readFileSync(__dirname + '/nps.html', 'utf-8'));
+app.get('/', (req, res) => res.send(fs.readFileSync(__dirname + '/index.html', 'utf-8')));
+app.get('/nps', (req, res) => rese.send(fs.readFileSync(__dirname + '/nps.html', 'utf-8')));
 
 var port = process.env.PORT || 3000;
 app.listen(port);
