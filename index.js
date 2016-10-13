61394e24a99a1bf7ae193ff9119418e7f4522de3
Matt Chadburn <matt.chadburn@ft.com>
2016-10-13 20:46:48 +0100

    Fetch

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ var express = require('express');
 var serveStatic = require('serve-static');
 var fs = require('fs');
 var app = express();
+var fetch = require('node-fetch');
 
 app.use(serveStatic(__dirname));
 
 app.get('/', (req, res) => res.send(fs.readFileSync(__dirname + '/index.html', 'utf-8')));
 app.get('/nps', (req, res) => res.send(fs.readFileSync(__dirname + '/nps.html', 'utf-8')));
+app.get('/fetch', (req, res) => {
+
+	fetch('https://errors-next.ft.com/errors/page/404')
+		.then(function(res) {
+			return res.text();
+		}).then(function(body) {
+			console.log(body);
+		});
+
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port);
